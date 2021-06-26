279e751b1d1abcbc0a2fb15b136c33fc9b39ae14
Patrick <pbobyn@gmail.com>
2021-06-26 14:46:23 -0400

    Improving the text reader. Will need more improvements

--- tests/test_read_receipt.py
+++ tests/test_read_receipt.py
@@ -1,5 +1,7 @@
-import pytest
+from read_receipt import get_image, read_img
 
 
-def test_test():
-    assert True, "This should not be found "
+def test_gets_text():
+    receipt = get_image(test=True)
+    data = read_img(receipt)
+    assert data, "This should have text"
