5f3ade881135fab7531592efa17bee18a49fd627
Patrick <pbobyn@gmail.com>
2021-06-26 14:46:23 -0400

    Improving the text reader. Will need more improvements

--- read_receipt.py
+++ read_receipt.py
@@ -1,28 +1,34 @@
 import cv2
+import imutils
 import pytesseract
 import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 # if Windows then set tesseract
+from PIL import Image
+
 if os.name == 'nt':
     pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
 
-def get_image():
+def get_image(test=False):
     """
     This method gets the image from a GUI
     :return: Returns the image
     """
-    # we don't want a full GUI, so keep the root window from appearing
-    Tk().withdraw()
+    if test:
+        file_name = os.getcwd() + '/img/template.jpeg'
+    else:
+        # we don't want a full GUI, so keep the root window from appearing
+        Tk().withdraw()
 
-    # opens up a in the "./img" directory so the user can choose the file via gui more options
-    # can be added like file extention filters and such but that can be added later /shrug
-    # show an "Open" dialog box and return the path to the selected file
-    file_name = askopenfilename(
-        initialdir=os.getcwd() + '/img',
-        title="Please select an image")
+        # opens up a in the "./img" directory so the user can choose the file via gui more options
+        # can be added like file extention filters and such but that can be added later /shrug
+        # show an "Open" dialog box and return the path to the selected file
+        file_name = askopenfilename(
+            initialdir=os.getcwd() + '/img',
+            title="Please select an image")
 
     return file_name
 
@@ -33,8 +39,32 @@ def read_img(file_name):
     :param file_name: The image to be read
     :return: Returns the text from the image
     """
+    # Read in image and store the tmp image
+    tmp_file = 'tmp.png'
     img = cv2.imread(file_name)
+
+    # Clean up image so it is easier to read
+    clean = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    clean = cv2.GaussianBlur(clean, (5, 5), 0)
+
+    # Show the Cleaned Image
+    cv2.imshow("Output", imutils.resize(clean, height=650))
+
+    # Waits for Image to be closed
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # Write cleaned image out to the tmp image
+    cv2.imwrite(tmp_file, clean)
+
+    # Open cleaned image and read txt
+    img = Image.open(tmp_file)
     text = pytesseract.image_to_string(img)
+
+    # Remove the tmp file
+    os.remove(tmp_file)
+
+    # return just the text
     return text
 
 
@@ -50,7 +80,6 @@ def parse_data(text):
 if __name__ == '__main__':
     receipt = get_image()
     data = read_img(receipt)
-
     items = parse_data(data)
-    for key, value in items:
-        print(f"{key}: {value}")
+
+    print(items)
